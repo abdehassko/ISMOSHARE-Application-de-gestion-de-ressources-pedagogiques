71303621ed55bce861bf55c5f25df7a92307a6ae
ABDERRAHIM EL HASSKOURI <rrahimabde033@gmail.com>
2025-12-14 19:08:25 +0100

    Update code-conexionAvecDB.php

--- others/code-conexionAvecDB.php
+++ others/code-conexionAvecDB.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db=new PDO("mysql:host=localhost;port=3306;dbname=ismoshare_db","root","1212",[PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION]);
+    $db=new PDO("mysql:host=localhost;port=3306;dbname=ismoshare_db","root","your_password",[PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION]);
 } catch (PDOException $e) {
     echo "Erreur Conexion".$e->getMessage();
 }
